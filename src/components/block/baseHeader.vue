0fce7d02b24d84f5817976795bc1cf713343bf1e
kritoma <toma.kristian97@gmail.com>
2022-06-07 14:55:54 +0200

    aggiuto header

---
+++ src/components/block/baseHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <div class="logo">
+          <img src="../../assets/img/spotify-logo.png" alt="logo-spotify">
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "baseHeader",
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    background-color: #2e3a46;
+    padding: 1.25rem;
+
+    img {
+        height: 3.125rem;
+    }
+}
+</style>
